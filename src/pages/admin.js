f7cbb9dc80f2708e22171c7b370df253d7524751
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-28 18:03:27 +0100

    add completedTask

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { StoreContext } from '../Context/store';
 
 const Admin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const { state } = useContext(StoreContext);
+  const { submitCompletedTasks } = state;
+
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Task Name</th>
+            <th>Pull Request Link</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {submitCompletedTasks.map((task) => (
+            <tr key={task.taskIndex}>
+              <td>
+                <a href={task.taskLink}>{task.taskName}</a>
+              </td>
+              <td>
+                <a href={task.pullRequestLink}>{task.pullRequestLink}</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Admin;
