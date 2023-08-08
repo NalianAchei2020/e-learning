54b4a27ef6f8c447340fb1cf5018c9bb7ace1ff7
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-08 12:43:25 +0100

    Update usersForm.js

--- client/src/pages/admin/usersForm.js
+++ client/src/pages/admin/usersForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react';
 import { StoreContext } from '../../Context/store';
 import axios from 'axios';
