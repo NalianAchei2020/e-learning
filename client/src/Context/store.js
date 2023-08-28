0a132c05795d4922305b438f3b91b2c31a3f9a5f
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-28 02:41:31 +0100

    Update store.js

--- client/src/Context/store.js
+++ client/src/Context/store.js
@@ -1,14 +1,49 @@
-import { createContext, useReducer } from "react";
-import reducer, {initialState} from "./reducer";
+import { createContext, useEffect, useReducer, useState } from 'react';
+import reducer, { initialState } from './reducer';
 
 export const StoreContext = createContext();
 
-export const StoreProvider = ({children}) => {
-const [state, dispatch] = useReducer(reducer, initialState);
+export const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [progress, setProgress] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [completedT, setCompletedT] = useState(0);
+  useEffect(() => {
+    localStorage.setItem('works', JSON.stringify(state.works));
+  }, [state.works]);
+  // Calculate progress percentage whenever works data changes
+  useEffect(() => {
+    const totalTasks = state.works.flatMap((module) =>
+      module.blocks.flatMap((block) => block.days.flatMap((day) => day.tasks))
+    ).length;
 
-return(
-<StoreContext.Provider value={{ state, dispatch }}>
-{children}
-</StoreContext.Provider>
-);
+    const completedTasks = state.works.flatMap((module) =>
+      module.blocks.flatMap((block) =>
+        block.days.flatMap((day) =>
+          day.tasks.filter((task) => task.status === 'Completed')
+        )
+      )
+    ).length;
+    setCompletedT(completedTasks);
+    setTotal(totalTasks);
+    const percentage = (completedTasks / totalTasks) * 100;
+    setProgress(percentage);
+  }, [state.works]);
+  const updateTaskStatus = (taskIndex, newStatus) => {
+    dispatch({
+      type: 'UPDATE_TASK_STATUS',
+      payload: {
+        taskIndex,
+        newStatus,
+      },
+    });
+  };
+
+  return (
+    <StoreContext.Provider
+      value={{ state, dispatch, updateTaskStatus, progress, total, completedT }}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
 };
